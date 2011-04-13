92c1bde1a01e61a0f06c0fb31390ba7392df708a
guohh <guoguowww818@126.com>
2011-04-13 08:09:31 +0000

    improve the form? 改进表单！ 2011年 04月 13日 星期三 16:09:28 CST

--- urls.py
+++ urls.py
@@ -18,6 +18,6 @@ urlpatterns = patterns('',
 	(r'^time/$',current_datetime),
 	(r'^time/plus/(\d{1,2})/$',hours_ahead),
 	(r'^meta/$',show_meta),
-	(r'^search_form/$',views.search_form),
+	(r'^search_form/$',views.search),
 	(r'^search/$',views.search),
 )
