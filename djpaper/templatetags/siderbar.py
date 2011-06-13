2d8c41fb7ddb4b378b43b417e8c293b864cdf564
guohh <guoguowww818@126.com>
2011-06-13 16:08:15 +0000

    great! use @register.inclusion_tag('',xxx) for tag-cloud and top-10-papers
    2011年 06月 14日 星期二 00:08:13 CST

---
+++ djpaper/templatetags/siderbar.py
@@ -0,0 +1,19 @@
+from django.template import Library
+from django import template
+from django.shortcuts import get_object_or_404
+from djpaper.models import Tag,Paper
+from djpaper.utils import tag_cloud_cal
+
+register = Library()
+
+@register.inclusion_tag('top_10.html',takes_context=True)
+def get_top_10_paper(context,number=10):
+	papers = Paper.objects.all()
+	if number > len(papers):
+		number = len(papers)
+	return {'top_10_papers':papers[:number]}
+
+@register.inclusion_tag('tag_cloud_page.html',takes_context=True)
+def get_tag_cloud(context):
+	tags = tag_cloud_cal()
+	return {'tags':tags}
